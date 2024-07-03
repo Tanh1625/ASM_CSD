a3ab89e6145fde8fa3da95521e35759c72ea4e63
Truong3102 <lekhatruong2004@gmail.com>
2024-07-03 16:38:09 +0700

    Update ArrayQueue.java

--- src/Graph/ArrayQueue.java
+++ src/Graph/ArrayQueue.java
@@ -3,87 +3,80 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Graph;
-
-/**
- *
- * @author ACER
- */
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author ACER
- */
-public class ArrayQueue<T> {
-    T array[];
+public class ArrayQueue <E> {
+    E a[];
     int size;
     int cap;
-    int front,back;
+    int fornt, back;
 
-    public ArrayQueue( int cap) {
-        this.cap = cap ;
-        this.array = (T[]) new Object[cap];
-        this.size = 0;
-        this.front = -1;
+    public ArrayQueue(int cap) {
+        this.cap = cap;
+        this.fornt = -1;
         this.back = -1;
-        
+        this.a = (E[]) new Object[cap];
+        this.size = 0;
     }
+    
     public boolean isFull(){
         return this.size == this.cap;
     }
+    
     public boolean isEmpty(){
         return this.size == 0;
     }
-    public void clear (){
-        this.array = (T[]) new Object[cap];
-        this.size = 0;
-        this.front = -1;
+    
+    public void clear(){
+        this.fornt = -1;
         this.back = -1;
+        this.a = (E[]) new Object[cap];
+        this.size = 0;
     }
-    public void enQueue(T data){
+    
+    public void enqueue(E data){
         if(this.isFull()){
             System.out.println("QUEUE IS FULL");
             return;
-        }if(this.isEmpty()){
-            this.front = this.back = 0;
-            this.array[this.back] = data;
-            this.size++;
+        }
+        if(this.isEmpty()){
+            this.fornt = this.back = 0;
+            this.a[this.back] =  data;
+            this.size ++;
             return;
         }
-        this.back = (this.back +1) % this.cap;
-        this.array[this.back] = data;
+        this.back = (this.back+1)%this.cap;
+        this.a[this.back] = data;
         this.size++;
     }
-    public T deQueue(){
+    
+    public E dequeue(){
         if(this.isEmpty()){
-            System.out.println("QUEUE IS EMPTY");
+            System.out.println("QUEUE IS EMPTY!");
             return null;
-        }if(this.size == 1){
-            T value = this.array[this.front];
+        }
+        if(this.size == 1){
+            E value = this.a[this.fornt];
             this.clear();
             return value;
         }
-        T value = this.array[this.front];
-        this.array[this.front] = null;
-        this.front = (this.front +1)%this.cap;
+        E value = this.a[this.fornt];
+        this.a[this.fornt] = null;
+        this.fornt = (this.fornt+1)%this.cap;
         this.size--;
         return value;
     }
-    public T front(){
+    
+    public E front(){
         if(this.isEmpty()){
-            System.out.println("QUEUE IS EMPTY");
+            System.out.println("QUEUE IS EMPTY!");
             return null;
         }
-        T value = this.array[this.front];
+        E value = this.a[this.fornt];
         return value;
     }
+    
     public void traverse(){
         for (int i = 0; i < this.size; i++) {
-            System.out.print(this.array[(this.front+i)%this.cap] + " ");
+            System.out.print (this.a[(this.fornt+i)%this.cap] + " ");
         }
     }
 }
