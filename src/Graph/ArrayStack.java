c52e0e0b7cadcedb8b9ad2e7a17deee5b146caa2
Truong3102 <lekhatruong2004@gmail.com>
2024-07-03 16:39:08 +0700

    Update ArrayStack.java

--- src/Graph/ArrayStack.java
+++ src/Graph/ArrayStack.java
@@ -3,78 +3,71 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Graph;
-
-/**
- *
- * @author ACER
- */
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-import java.util.EmptyStackException;
-
 import java.util.EmptyStackException;
 
 /**
  *
- * @author ACER
+ * @author IDEAPAD
  */
-public class ArrayStack <T>{
-    T arr[];
+public class ArrayStack <E>{
+    E arr[];
     int size;
     int top;
     int cap;
 
-    public ArrayStack( int cap) {
-       
-        this.cap = cap;
-        this.arr = (T[])new Object[cap];
-        this.top = -1;
+    public ArrayStack(int cap) {
+        this.arr =(E[]) new Object[cap];
         this.size = 0;
+        this.top = -1;
+        this.cap = cap;
     }
+    
     public boolean isFull(){
         return this.size == cap;
     }
+    
     public boolean isEmpty(){
         return this.size == -1;
     }
-    public void look(){
+    
+    public void look (){
         for (int i = 0; i < this.size; i++) {
-            System.out.println(arr[top-i] + " ");
+            System.out.print(arr[top-i] + " ");
         }
     }
-    public void push(T data){
+    
+    public void push(E data){
         if(isFull()){
-            throw new ArrayStoreException("Stack is full,cannot push ");
+            throw new ArrayStoreException("Stack is full, cannot push");
         }
         this.top++;
-        this.arr[top] = data;
+        this.arr[top]=data;
         this.size++;
     }
-    public T pop(){
+    
+    public E pop(){
         if(isEmpty()){
             throw new EmptyStackException();
         }
-        T data = arr[top];
+        E data = arr[top];
         this.arr[top] = null;
         this.top--;
         this.size--;
         return data;
-        }
+    }
     
-    public T peek(){
+    public E peek(){
         if(isEmpty()){
             throw new EmptyStackException();
         }
         return arr[top];
     }
+    
     public void clear(){
-        this.arr = (T[])new Object[cap];
-        this.top = -1;
+        this.arr =(E[]) new Object[cap];
         this.size = 0;
+        this.top = -1;
     }
+    
 }
 
